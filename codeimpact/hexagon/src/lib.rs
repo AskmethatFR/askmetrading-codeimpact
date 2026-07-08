95fad11b7193d360a832e90c43694bba5b0ea904
Alexandre Teixeira <ateixeira@askmethat.fr>
2026-07-08 15:12:22 +0200

    chore: remove code scaffold, keep only docs

--- codeimpact/hexagon/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod domain_model;
-
-pub mod gateways_secondary_ports;
-pub mod use_cases_application_services;
-
-pub use domain_model::*;
-pub use gateways_secondary_ports::*;
-pub use use_cases_application_services::*;
